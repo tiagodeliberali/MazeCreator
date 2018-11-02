df8e8d6ad583b1de0230ac4342027f3e821cecae
Tiago Deliberali <tiago.deliberali@gmail.com>
2018-11-02 09:56:29 -0300

    Refactored maze draw

--- maze.main/Program.cs
+++ maze.main/Program.cs
@@ -5,6 +5,10 @@ namespace MazeBuilderGame
 {
     class Program
     {
+        private static string[] TOP_CORNERS = new string[] { "╔", "╗", "╝", "╚" };
+
+        private static string[] BOTTON_CORNERS = new string[] { "╚", "╝", "╗", "╔" };
+
         static void Main(string[] args)
         {
             MazeSlot[,] maze = BuildMaze();
@@ -14,75 +18,68 @@ namespace MazeBuilderGame
             {
                 for (int x = 0; x < maze.GetLength(0); x++)
                 {
-                    if (!maze[x, y].DestroyedWalls.Contains(WallPosition.Up))
-                    {
-                        if (!maze[x, y].DestroyedWalls.Contains(WallPosition.Left)) Console.Write("╔");
-                        else Console.Write("═");
-
-                        Console.Write("═══");
-
-                        if (!maze[x, y].DestroyedWalls.Contains(WallPosition.Rigth)) Console.Write("╗");
-                        else Console.Write("═");
-                    }
-                    else
-                    {
-                        if (!maze[x, y].DestroyedWalls.Contains(WallPosition.Left)) Console.Write("║");
-                        else Console.Write("╝");
-
-                        Console.Write("   ");
-
-                        if (!maze[x, y].DestroyedWalls.Contains(WallPosition.Rigth)) Console.Write("║");
-                        else Console.Write("╚");
-                    }
+                    DrawHorizontal(maze, y, x, WallPosition.Up);
                 }
                 Console.WriteLine();
 
                 for (int x = 0; x < maze.GetLength(0); x++)
                 {
-                    if (!maze[x, y].DestroyedWalls.Contains(WallPosition.Left))
-                        Console.Write("║");
-                    else
-                        Console.Write(" ");
-
-                    if (maze[x, y].SolutionPath)
-                        Console.Write(" X ");
-                    else
-                        Console.Write("   ");
-
-                    if (!maze[x, y].DestroyedWalls.Contains(WallPosition.Rigth))
-                        Console.Write("║");
-                    else
-                        Console.Write(" ");
+                    DrawVertical(maze, y, x);
                 }
                 Console.WriteLine();
 
                 for (int x = 0; x < maze.GetLength(0); x++)
                 {
-                    if (!maze[x, y].DestroyedWalls.Contains(WallPosition.Down))
-                    {
-                        if (!maze[x, y].DestroyedWalls.Contains(WallPosition.Left)) Console.Write("╚");
-                        else Console.Write("═");
-
-                        Console.Write("═══");
-
-                        if (!maze[x, y].DestroyedWalls.Contains(WallPosition.Rigth)) Console.Write("╝");
-                        else Console.Write("═");
-                    }
-                    else
-                    {
-                        if (!maze[x, y].DestroyedWalls.Contains(WallPosition.Left)) Console.Write("║");
-                        else Console.Write("╗");
-
-                        Console.Write("   ");
-
-                        if (!maze[x, y].DestroyedWalls.Contains(WallPosition.Rigth)) Console.Write("║");
-                        else Console.Write("╔");
-                    }
+                    DrawHorizontal(maze, y, x, WallPosition.Down);
                 }
                 Console.WriteLine();
             }
 
             Console.ReadLine();
+        }
+
+        private static void DrawVertical(MazeSlot[,] maze, int y, int x)
+        {
+            if (!maze[x, y].DestroyedWalls.Contains(WallPosition.Left))
+                Console.Write("║");
+            else
+                Console.Write(" ");
+
+            if (maze[x, y].SolutionPath)
+                Console.Write(" X ");
+            else
+                Console.Write("   ");
+
+            if (!maze[x, y].DestroyedWalls.Contains(WallPosition.Rigth))
+                Console.Write("║");
+            else
+                Console.Write(" ");
+        }
+
+        private static void DrawHorizontal(MazeSlot[,] maze, int y, int x, WallPosition wall)
+        {
+            string[] corners = wall == WallPosition.Up ? TOP_CORNERS : BOTTON_CORNERS;
+
+            if (!maze[x, y].DestroyedWalls.Contains(wall))
+            {
+                if (!maze[x, y].DestroyedWalls.Contains(WallPosition.Left)) Console.Write(corners[0]);
+                else Console.Write("═");
+
+                Console.Write("═══");
+
+                if (!maze[x, y].DestroyedWalls.Contains(WallPosition.Rigth)) Console.Write(corners[1]);
+                else Console.Write("═");
+            }
+            else
+            {
+                if (!maze[x, y].DestroyedWalls.Contains(WallPosition.Left)) Console.Write("║");
+                else Console.Write(corners[2]);
+
+                Console.Write("   ");
+
+                if (!maze[x, y].DestroyedWalls.Contains(WallPosition.Rigth)) Console.Write("║");
+                else Console.Write(corners[3]);
+            }
         }
 
         private static MazeSlot[,] BuildMaze()
